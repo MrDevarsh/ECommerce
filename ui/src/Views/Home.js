1f33263f569f5bbf1b1c8883af992f54d6e074b8
MrDevarsh <dvasa40@gmail.com>
2024-01-26 01:55:24 +0530

    Created pages for ui

---
+++ ui/src/Views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Home() {
+  return (
+    <div>
+        <div>
+            <Navbar/>
+        </div>
+        <hr/>
+        <div>Home</div>
+    </div>
+  );
+}
+
+export default Home;
